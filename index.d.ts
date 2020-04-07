a69a6851b5ff87655b334a175cc80d8c834b28f8
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-04-07 18:47:47 -0500

    fix return type

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,13 @@
+import { Debugger } from 'debug'
+
 declare module 'create-debug' {
   export interface Configuration {
     rootDir: string
     prefix: string
   }
 
-  export function createDebug(module: NodeModule, config: Configuration): void
+  export function createDebug(
+    module: NodeModule,
+    config: Configuration
+  ): Debugger
 }
